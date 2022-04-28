4aa4cfd09b7846770f32cad1dd5f85db86583876
KingCorvus <gus.aquino42@gmail.com>
2022-04-28 15:19:11 -0300

    Batalha Espacial - Incompleto

--- VPL/Batalha Espacial/Naves.cpp
+++ VPL/Batalha Espacial/Naves.cpp
@@ -1,12 +1,18 @@
 #include "Naves.hpp"
 
-Nave::Nave(Ponto2D posicao, double forca){
+Nave::Nave(Ponto2D posicao, double forca){ //caso o usiário coloque foca = 0
     _posicao = posicao;
     if(forca != 0){
         _forca = forca;
     }else{
         _forca = 1.0;
     }
+    _energia = 100.0;
+}
+
+Nave::Nave(Ponto2D posicao){ //caso o usuário simplesmente não coloque a variavel foca
+    _posicao = posicao;
+    _forca = 1.0;
     _energia = 100.0;
 }
 
@@ -21,3 +27,26 @@ void Nave::mover(double dx, double dy){
     _posicao.set_y(dy);
 }
 
+double calcular_distancia(Nave nave){
+    double dx = (nave.get_posicao_x() - this->get_posicao_x()); 
+    double dy = (nave.get_posicao_y() - this->get_posicao_y());
+}
+
+int Nave::determinar_indice_nave_mais_proxima(Nave naves[], int n){
+    int aux; //variavel auxiliar que recebe o indice da nave mais próxima
+    for (int i = 0; i < n; i++){
+        this->
+    }
+    
+}
+
+
+
+//Métodos Adicionais
+double Nave::get_posicao_x(){
+    return _posicao.get_x();
+}
+    
+double Nave::get_posicao_y(){
+    return _posicao.get_y();
+}
---
+++ VPL/Batalha Espacial/Naves.hpp
@@ -0,0 +1,25 @@
+#ifndef NAVES_HPP   
+#define NAVES_HPP   
+
+#include "Ponto2D.hpp"
+
+class Nave{
+    private:
+        Ponto2D _posicao; double _forca; double _energia;
+    public:
+        Nave(Ponto2D posicao, double forca);
+        Nave(Ponto2D posicao);
+        Nave();
+        void mover(double dx, double dy);
+        double calcular_distancia(Nave nave);
+        int determinar_indice_nave_mais_proxima(Nave naves[], int n);
+        void atacar(Nave naves[], int n);
+        void imprimir_status();
+
+        //Métodos Adicionais
+        double get_posicao_x();
+        double get_posicao_y();
+};      
+
+
+#endif
